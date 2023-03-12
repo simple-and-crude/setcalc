9857ce5fa058df741db77656a59a01edd3ab88ad
E0SelmY4V-PC <2507917746@qq.com>
2023-03-12 19:51:42 +0800

    Env 使用更新版本的《死亡构建》

--- dobuild.js
+++ dobuild.js
@@ -1,21 +1,15 @@
-const { snake, exec, dels, cps, outFS, log } = require('lethal-build')(__dirname);
+const { snake, exec, cmt, dels, cps, outFS, log } = require('lethal-build')(__dirname);
 
 snake(
 	exec('tsc'),
 	cps([['lib/exp.js', 'types/exp.js']]),
 	exec('webpack'),
 	outFS([
+		[1, cmt('lib/index.ts')],
 		[1, '!function(exp){'],
 		[0, 'types/packed.js'],
 		[1, '}(typeof module==="undefined"?false:module)']
 	], 'index.js'),
-	dels([
-		'types/calc.js',
-		'types/tool.js',
-		'types/rpnify.js',
-		'types/exp.js',
-		'types/index.js',
-		'types/packed.js',
-	]),
+	dels(/types[\/\\].*js$/),
 	log('OK.')
 );
